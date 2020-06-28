0f8c802f5d5d2013bf70753e15945df56102610b
Yoshihiro Tanaka <contact@cordea.jp>
2020-06-28 13:10:09 +0900

    define new command

---
+++ newcmd.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/google/subcommands"
+)
+
+type newCmd struct {
+	name string
+}
+
+func (*newCmd) Name() string {
+	return ""
+}
+
+func (*newCmd) Synopsis() string {
+	return ""
+}
+
+func (*newCmd) Usage() string {
+	return ""
+}
+
+func (n *newCmd) SetFlags(f *flag.FlagSet) {
+	f.StringVar(&n.name, "name", "", "name")
+}
+
+func (n *newCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	return subcommands.ExitSuccess
+}
