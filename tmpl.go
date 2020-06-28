df4a6aac38238f407bf88dc9e08ac3c5a876333b
Yoshihiro Tanaka <contact@cordea.jp>
2020-06-28 12:54:49 +0900

    parse commands

--- tmpl.go
+++ tmpl.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"github.com/google/subcommands"
+	"os"
+)
 
+func main() {
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+
+	flag.Parse()
+	os.Exit(int(subcommands.Execute(context.Background())))
 }
