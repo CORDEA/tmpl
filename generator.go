689e7ed72e05e59e10b2d5cf992257c87dd5c8ab
Yoshihiro Tanaka <contact@cordea.jp>
2020-06-28 18:12:40 +0900

    list files

--- generator.go
+++ generator.go
@@ -1,9 +1,32 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type generator struct {
 	templatePath string
 	args         map[string]string
 }
 
 func (g *generator) generate() {
+	g.listFiles()
+}
+
+func (g *generator) listFiles() []string {
+	var files []string
+	err := filepath.Walk(g.templatePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
